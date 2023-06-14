4ff2c19933c262a6b028c99e5ef03615fdb81501
jeferson.arboleda <jeferson.arboleda@pragma.com.co>
2023-06-14 15:56:52 -0500

    Route parameters

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ app = FastAPI()
 app.title = "Mi aplicación con FastApi" #Title in docs
 app.version = "0.0.1"
 
-movies = [
-    {
+movies = {
+    1:{
         'id': 1,
         'title': 'Avatar',
         'overview': 'En un exuberante planeta...',
         'year': '2009',
         'rating': 7.8,
         'category': 'Acción'
+    },
+    2:{
+        'id': 2,
+        'title': 'Avatar',
+        'overview': 'En un exuberante planeta...',
+        'year': '2009',
+        'rating': 7.8,
+        'category': 'Acción'
     }
-]
+}
 
 @app.get('/', tags=['home']) #Tags change method title in docs
 def message():
@@ -24,3 +32,9 @@ def message():
 def get_movies():
     return movies
     
+@app.get('/movies/{id}', tags=['movies'])
+def get_movies(id: int):
+    if id in movies.keys():
+        return movies[id] 
+    else:
+        return []
